a5afb62f14cbeb8716798f1c5c1fc211d5a51914
Diego Monsalve <diales1981@gmail.com>
2023-07-23 14:23:06 -0500

    Create a building component

--- src/pages/Modals.tsx
+++ src/pages/Modals.tsx
@@ -32,6 +32,7 @@ export const Modals = () => {
                   backgroundColor={button.backgroundColor}
                   hasBackground={button.hasBackground}
                   onClick={() => openModal(button.onShowModal!)}
+                  size="medium"
                 />
               ))
             }
@@ -57,6 +58,7 @@ export const Modals = () => {
                   backgroundColor={button.backgroundColor}
                   hasBackground={button.hasBackground}
                   onClick={() => openModal(button.onShowModal!)}
+                  size="medium"
                 />
               ))
             }
