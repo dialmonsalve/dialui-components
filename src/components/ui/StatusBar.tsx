6d638bbfa1b836905e59fefe946aaf4cb86168e7
Diego Monsalve <diales1981@gmail.com>
2024-01-26 23:46:12 -0500

    Creating StatusBar

---
+++ src/components/ui/StatusBar.tsx
@@ -0,0 +1,6 @@
+
+export const StatusBar = () => {
+  return (
+    <div className="statusbar" ></div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { router } from './router/App.tsx';
 import './index.scss';
 import { RouterProvider } from 'react-router-dom';
 import { ProviderHandlerUI } from './context/ui/HandlerUIProvider.tsx';
+import { StatusBar } from './components/ui/StatusBar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <StatusBar />
     <ProviderHandlerUI >
       {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
       <RouterProvider router={router} />
