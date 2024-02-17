642e6353f4cd0802017d7b8f6d358ca968348593
Diego Monsalve <diales1981@gmail.com>
2024-02-17 14:56:11 -0500

    Fix paths

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from "./components/form"
+export * from "./components/UI"
+
+export {ModalProvider} from "./context/modal/ModalProvider"
+export {UIProvider} from "./context/ui/UIProvider"
+
+export * from "./hooks/context"
+
+export * from "./hooks/form"
+export * from "./hooks"
+
