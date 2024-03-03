b30e52b8e4b8830ff06858a5e9f79e05628996d2
Diego Monsalve <diales1981@gmail.com>
2024-03-02 19:51:00 -0500

    Clean scss files

--- src/components/UI/alert/Alert.tsx
+++ src/components/UI/alert/Alert.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from 'react';
 
 import type { MessageType } from '../../../types';
-import { HASH } from '@/utils/hash';
+import { HASH } from '../../../utils/hash';
 
-import '@prodStyles/UI/alert.css';
+import '../../../styles/components/UI/alert.css';
 
 interface Props {
 	message: string;
